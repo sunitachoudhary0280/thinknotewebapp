8c4852f1586d1a8ff4435fe1630d05145eba4f0d
Sunita Choudhary <124172022+sunitachoudhary0280@users.noreply.github.com>
2024-12-06 22:55:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,18 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
